d21f2e36ce7537f02dafae5d52595c5b58196d7f
Greg Adler <greg7239@gmail.com>
2019-08-04 11:57:27 -0700

    initial commit

---
+++ mvcdocker/Controllers/SubmitController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mvcdocker
+{
+    public class SubmitController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+
+        // GET: /submit/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: submit/ReceiveUser
+        public async Task<HttpResponseMessage> ReceiveUser()
+        {
+            ArrayList userParams = new ArrayList(2);
+            using (var reader = new StreamReader(Request.Body))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    userParams.Add(line);
+
+                }
+            }
+            var response = await PostUser(userParams);
+
+            if (response.StatusCode.ToString() == "OK"){
+                Console.WriteLine("Success");
+                // Receive Success back to angular
+                // var angularCall = await ReturnUser("Success");
+                return response;
+            }
+            else
+            {
+                Console.WriteLine("Failure");
+                // Return Failure back to angular
+                // var angularCall = await ReturnUser("Failure");
+                return response;
+            }
+        }
+
+        public async Task<HttpResponseMessage> PostUser(ArrayList userParams)
+        {
+
+            var values = new Dictionary<string, string>
+            {
+                 { "Username", userParams[0].ToString()},
+                 { "EmailAddress", userParams[1].ToString() },
+                 { "Password", userParams[2].ToString() },
+                 { "ReferredBy",  "a" },
+                 { "RegistrationChannel", "the"  },
+                 { "CreateRegistrationToken" , "No" },
+                 {"CodeChallenge", "yes" },
+                 { "SendRegistrationEmail", "No" }
+             };
+
+
+            foreach (KeyValuePair<string, string> pair in values)
+            {
+                Console.WriteLine("{0}, {1}", pair.Key, pair.Value);
+            }
+
+            var content = new FormUrlEncodedContent(values);
+
+            Console.WriteLine("API CALL");
+
+            var response = await client.PostAsync("https://developer.skidata-loyalty.com/user/82/v1/user", content);
+
+            // var response = await client.PostAsync("https://testportal.skidataus.com/user/82/v1/user", content);
+
+            Console.WriteLine("response");
+
+            Console.WriteLine(response);
+
+            // string responseBody = await response.Content.ReadAsStringAsync();
+            // Console.WriteLine("content");
+            //  Console.WriteLine(responseBody);
+
+            Console.WriteLine("Response StatusCode");
+            Console.WriteLine(response.StatusCode);
+
+            return response;
+
+        }
+    }
+}
---
+++ mvcdocker/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace mvcdocker
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200");
+                });
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
